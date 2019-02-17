bcd1a9f5859e42ed829103a1407f5525ae7fbd8a
Brittany Ohalete <bopho@Brittanys-MacBook-Pro.local>
2019-02-16 21:41:41 -0500

    Create listening socket

--- server.c
+++ server.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdlib.h>
@@ -15,4 +16,13 @@ int main(){
   int conn_s;
   struct sockaddr_in servaddr;
   char buffer[MAX_LINE];
+
+  // create and check listening Socket
+  list_s = socket(AF_INET, SOCK_STREAM, 0);
+  if(list_s < 0){
+    printf("Failure with socket creation \n");
+    exit(0);
+  }
+  else
+    printf("Succesful socket creation \n");
 }
