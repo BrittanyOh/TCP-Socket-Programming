3ed5d08e4aa2cb8a67e347184b8cd6a2f597256f
Brittany Ohalete <bopho@Brittanys-MacBook-Pro.local>
2019-02-16 21:59:56 -0500

    Bind socket address to listening socket

--- server.c
+++ server.c
@@ -20,15 +20,23 @@ int main(){
   // create and check listening Socket
   list_s = socket(AF_INET, SOCK_STREAM, 0);
   if(list_s < 0){
-    printf("Error creating listening socket \n");
+    fprintf(stderr, "ECHOSERV: Error creating listening socket\n");
     exit(EXIT_FAILURE);
   }
   else
-    printf("Succesful socket creation \n");
+    printf("Socket was successfully created! \n");
 
     //set byes in socket address to 0, assign IP and PORT
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(PORT);
+
+    //bind socket address to listening Socket
+    if(bind(list_s, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0){
+      fprintf(stderr, "ECHOSERV: Error calling bind()\n");
+      exit(EXIT_FAILURE);
+    }
+    else
+      printf("Socket was successfully binded! \n");
 }
